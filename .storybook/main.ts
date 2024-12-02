e846cf07869908ffd6fe55c2b9fd3641fd8eb347
stelco <stec74@gmail.com>
2024-12-02 15:06:22 +0000

    public dir fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,6 +14,6 @@ const config: StorybookConfig = {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  staticDirs: ["..\\public"],
+  staticDirs: ["../public"], // Correct the path to the public directory
 };
 export default config;
