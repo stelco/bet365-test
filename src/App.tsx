6eb1114daaa1bb5cc2b599c018a8657fdb815605
stelco <stec74@gmail.com>
2024-12-02 15:26:28 +0000

    added storybook link

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,18 @@ import { cardData } from './data/CardData';
 function App() {
   return (
     <div>
+      <div className='header'>
+        <h1>Card Component</h1>
+        <p>
+          Storybook documentation is{' '}
+          <a href="https://bet365-test-lnyhhflo4-steven-collins-projects.vercel.app/" target="_blank" rel="noopener noreferrer">
+            available here
+          </a>
+        </p>
+      </div>
+
       <main className='container'>
-          <Card cardData={cardData} />
+        <Card cardData={cardData} />
       </main>
     </div>
   );
